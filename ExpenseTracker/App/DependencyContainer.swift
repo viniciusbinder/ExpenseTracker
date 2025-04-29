113a6225fd63522c72b702ce9c0345aef31a0706
Vinícius Binder <vinicius.binder@gmail.com>
2025-04-29 15:39:18 -0300

    Set up dependency container

---
+++ ExpenseTracker/App/DependencyContainer.swift
@@ -0,0 +1,47 @@
+//
+//  DependencyContainer.swift
+//  ExpenseTracker
+//
+//  Created by Vinícius on 29/04/25.
+//
+
+import Foundation
+
+final class DependencyContainer {
+    #if DEBUG
+        private let storage = InMemoryStorage()
+    #else
+        private let storage = SwiftDataStorage()
+    #endif
+
+    private let expenseRepository: ExpenseRepository
+    private let categoryRepository: CategoryRepository
+    private let expenseService: ExpenseService
+    private let categoryService: CategoryService
+
+    init() {
+        self.expenseRepository = ExpenseRepositoryImpl(dataSource: storage)
+        self.categoryRepository = CategoryRepositoryImpl(dataSource: storage)
+
+        self.expenseService = ExpenseService(
+            expenseRepository: expenseRepository,
+            categoryRepository: categoryRepository
+        )
+        self.categoryService = CategoryService(
+            categoryRepository: categoryRepository,
+            expenseRepository: expenseRepository
+        )
+    }
+}
+
+// MARK: ViewModel Factory
+@MainActor
+extension DependencyContainer {
+    func makeExpenseListViewModel() -> ExpenseListViewModel {
+        ExpenseListViewModel(service: expenseService)
+    }
+
+    func makeCategoryListViewModel() -> CategoryListViewModel {
+        CategoryListViewModel(service: categoryService)
+    }
+}
--- ExpenseTracker/App/ExpenseTrackerApp.swift
+++ ExpenseTracker/App/ExpenseTrackerApp.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 @main
 struct ExpenseTrackerApp: App {
+    private let dependencies: DependencyContainer = .init()
+
     var body: some Scene {
         WindowGroup {
-            ExpenseListScreen(viewModel: .init(service: .mock()))
+            TabView {
+                ExpenseListScreen(viewModel: dependencies.makeExpenseListViewModel())
+                    .tabItem {
+                        Label("Expenses", systemImage: "list.bullet")
+                    }
+
+                CategoryListScreen(viewModel: dependencies.makeCategoryListViewModel())
+                    .tabItem {
+                        Label("Categories", systemImage: "folder")
+                    }
+            }
         }
     }
 }
